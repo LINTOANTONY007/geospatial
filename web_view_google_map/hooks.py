964250a0fb58eac4b9b73be11a8f737afb0d6f45
Maxime Chambreuil <maxime.chambreuil@graymatterlogic.com>
2026-07-25 17:24:43 -0600

    [MIG] web_view_google_map: Migration to 19.0

--- web_view_google_map/hooks.py
+++ web_view_google_map/hooks.py
@@ -2,15 +2,15 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
-def uninstall_hook(cr, registry):
-    cr.execute(
+def uninstall_hook(env):
+    env.cr.execute(
         "UPDATE ir_act_window "
-        "SET view_mode=replace(view_mode, ',google_map', '')"
-        "WHERE view_mode LIKE '%,google_map%';"
+        "SET view_mode=replace(view_mode, ',google_map', '') "
+        "WHERE view_mode LIKE '%,google_map%'"
     )
-    cr.execute(
+    env.cr.execute(
         "UPDATE ir_act_window "
-        "SET view_mode=replace(view_mode, 'google_map,', '')"
-        "WHERE view_mode LIKE '%google_map,%';"
+        "SET view_mode=replace(view_mode, 'google_map,', '') "
+        "WHERE view_mode LIKE '%google_map,%'"
     )
-    cr.execute("DELETE FROM ir_act_window WHERE view_mode = 'google_map';")
+    env.cr.execute("DELETE FROM ir_act_window WHERE view_mode = 'google_map'")
