1490a0f3605b4ce7728cd3e78defe01374c7e2b4
linto-antony <lintoantonyv@gmail.com>
2026-08-07 13:51:34 +0530

    [IMP] website_geoengine_store_locator: pre-commit auto fixes

--- website_geoengine_store_locator/static/src/js/snippets/s_openlayer_store_locator/snippet.options.esm.js
+++ website_geoengine_store_locator/static/src/js/snippets/s_openlayer_store_locator/snippet.options.esm.js
@@ -1,5 +1,8 @@
 /** @odoo-module **/
-import options from "web_editor.snippets.options";
+
+/* global console */
+/* global ol */
+import options from "@web_editor/js/editor/snippets.options";
 
 /**
  * Copyright 2011-2024 Camptocamp SA
@@ -8,19 +11,13 @@ import options from "web_editor.snippets.options";
 
 options.registry.OpenLayerStoreLocator = options.Class.extend({
     jsLibs: [
-        "/web/static/lib/Chart/Chart.js",
         "/website_geoengine_store_locator/static/lib/node_modules/ol/dist/ol.js",
         "/website_geoengine_store_locator/static/lib/node_modules/jquery-flexdatalist/jquery.flexdatalist.js",
     ],
     cssLibs: [
-        "/website_geoengine_store_locator/static/styles.css",
         "/website_geoengine_store_locator/static/lib/node_modules/ol/ol.css",
         "/website_geoengine_store_locator/static/lib/node_modules/jquery-flexdatalist/jquery.flexdatalist.css",
     ],
-
-    init() {
-        return this._super.apply(this, arguments);
-    },
 
     async onBuilt() {
         this._super.apply(this, arguments);
@@ -71,7 +68,12 @@ options.registry.OpenLayerStoreLocator = options.Class.extend({
     },
 
     cleanForSave() {
-        this.mapElement.innerHTML = "";
+        // The editor can clean an option after its target has been rebuilt
+        // (for example, when a snippet is dragged and dropped). In that case
+        // `onBuilt` may not have initialized `mapElement` yet.
+        if (this.mapElement) {
+            this.mapElement.innerHTML = "";
+        }
     },
 });
 
